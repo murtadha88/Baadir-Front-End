3b1742dc063aa2f369dbcf3e5f2406ff00c2d623
Murtadha Almoamen <murtadhaalmoamen8@gmail.com>
2025-02-25 20:31:03 +0300

    remove the deplucate

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -2,10 +2,9 @@ import { useState, useContext } from 'react';
 import { useNavigate } from 'react-router';
 
 import { signIn } from '../../services/authService';
-
 import { UserContext } from '../../contexts/UserContext';
-import '../../css/SignInForm.css'
-import '../../css/SignInForm.css'
+
+import '../../css/SignInForm.css';
 
 import auth from "../../images/auth-image.png";
 import Logo from "../../images/Logo.png";
@@ -28,9 +27,8 @@ const SignInForm = () => {
     evt.preventDefault();
     try {
       const signedInUser = await signIn(formData);
-
       await setUser(signedInUser);
-      navigate('/')
+      navigate('/');
     } catch (err) {
       setMessage(err.message);
     }
@@ -40,57 +38,45 @@ const SignInForm = () => {
     <div className='signin-container'>
       <div className='signin-left'>
         <img id='baadir-logo' src={Logo} alt="Baadir logo" />
-        <p id='slogan'>Unlock your fun experience with <br></br>every opportunity you take!</p>
+        <p id='slogan'>Unlock your fun experience with <br />every opportunity you take!</p>
         <img id='volunteer-illustration' src={auth} alt="volunteer-illustration" />
       </div>
+
       <div className='signin-right'>
-    <div className='signin-container'>
-      <div className='signin-left'>
-        <img id='baadir-logo' src="src/images/Logo.png" alt="Baadir logo" />
-        <p id='slogan'>Unlock your fun experience with <br></br>every opportunity you take!</p>
-        <img id='volunteer-illustration' src="src/images/auth-image.png" alt="volunteer-illustration" />
-      </div>
-      <div className='signin-right'>
-      <p>{message}</p>
-      <form className='signin-form' autoComplete='off' onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor='email'>email</label>
-          <label htmlFor='email'>email</label>
-          <input
-            type='text'
-            autoComplete='off'
-            id='email'
-            value={formData.email}
-            name='email'
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor='password'>password</label>
-          <label htmlFor='password'>password</label>
-          <input
-            type='password'
-            autoComplete='off'
-            id='password'
-            value={formData.password}
-            name='password'
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <button className='signin-button'>Sign In</button>
-          {/* <button onClick={() => navigate('/')}>Cancel</button> */}
-          <p>Don't have an account? <a href='/sign-up' className='signin-link'>Sign up</a></p>
-        </div>
-      </form>
+        <p>{message}</p>
+        <form className='signin-form' autoComplete='off' onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor='email'>Email</label>
+            <input
+              type='text'
+              autoComplete='off'
+              id='email'
+              value={formData.email}
+              name='email'
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor='password'>Password</label>
+            <input
+              type='password'
+              autoComplete='off'
+              id='password'
+              value={formData.password}
+              name='password'
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <button className='signin-button'>Sign In</button>
+            <p>Don't have an account? <a href='/sign-up' className='signin-link'>Sign up</a></p>
+          </div>
+        </form>
       </div>
     </div>
-    );
-      </div>
-    </div>
-    );
+  );
 };
 
-export default SignInForm
+export default SignInForm;
